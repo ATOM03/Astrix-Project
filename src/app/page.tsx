d8cb74940dd0d81b211a63ffd42218a066c3fb78
ATOM03 <priyanshujhaginia@outlook.com>
2024-05-18 19:47:18 +0530

    added the carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,52 @@ import Image from "next/image";
 import Header from "./components/Header/page";
 import { useState } from "react";
 import AnimatedTabs from "./components/Tabs/page";
+import DemoCarousel from "./components/Carousel/page";
+import Carousel from "./components/Carousel/page";
 
 export default function Home() {
   const [active, setActive] = useState("tab1");
+  const countries = [
+    {
+      image: "/brazil.jpg",
+      title: "Brazil",
+    },
+    {
+      image: "/china.jpg",
+      title: "China",
+    },
+    // {
+    //   image: "/images/france.jpg",
+    //   title: "France",
+    // },
+    // {
+    //   image: "/images/japan.jpg",
+    //   title: "Japan",
+    // },
+    // {
+    //   image: "/images/norway.jpg",
+    //   title: "Norway",
+    // },
+  ];
 
   return (
     <main className="w-screen h-screen">
       <div>
         <Header />
-        <div className="flex flex-col  justify-center ml-16">
-          <div className="mt-24">
-            <div className="opacity-20 text-9xl font-extrabold ">ASTR </div>
-            <div className="opacity-10 text-9xl font-extrabold ">IX</div>
-            <div className="mt-32 opacity-20 text-9xl font-extrabold ">
-              EVE{" "}
+        <div>
+          <div>
+            <Carousel images={countries} />
+          </div>
+
+          <div className="flex flex-col  justify-center ml-16">
+            <div className="mt-24">
+              <div className="opacity-20 text-9xl font-extrabold ">ASTR </div>
+              <div className="opacity-10 text-9xl font-extrabold ">IX</div>
+              <div className="mt-32 opacity-20 text-9xl font-extrabold ">
+                EVE{" "}
+              </div>
+              <div className="opacity-10 text-9xl font-extrabold ">NTS</div>
             </div>
-            <div className="opacity-10 text-9xl font-extrabold ">NTS</div>
           </div>
         </div>
         <div className="mt-14">
