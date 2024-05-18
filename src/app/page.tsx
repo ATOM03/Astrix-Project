a57d36785cce81b5d792f611ccc4f0d6ce3fdc58
ATOM03 <priyanshujhaginia@outlook.com>
2024-05-18 14:33:11 +0530

    Animated Tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Header from "./components/Header/page";
 import { useState } from "react";
+import AnimatedTabs from "./components/Tabs/page";
 
 export default function Home() {
   const [active, setActive] = useState("tab1");
@@ -21,22 +22,7 @@ export default function Home() {
             <div className="opacity-10 text-9xl font-extrabold ">NTS</div>
           </div>
         </div>
-        <div role="tablist" className="tabs tabs-boxed">
-          <a
-            role="tab"
-            className={active === "tab1" ? "tab tab-active" : "tab"}
-            onClick={() => setActive("tab1")}
-          >
-            Tab 1
-          </a>
-          <a
-            role="tab"
-            className={active === "tab2" ? "tab tab-active" : "tab"}
-            onClick={() => setActive("tab2")}
-          >
-            Tab 2
-          </a>
-        </div>
+        <AnimatedTabs />
       </div>
     </main>
   );
